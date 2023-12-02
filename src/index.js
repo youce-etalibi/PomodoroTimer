de3d9df51352335f9cbe8d0c8e67ea4d320ddfcf
Youssef Talibi <youssef.talibi11@gmail.com>
2023-12-02 13:39:55 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Accueil from './Components/Accueil';
+import { Provider } from 'react-redux';
+import store from './Components/Redux/Store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Fragment>
+      <Provider store={store}>
+        <Accueil />
+      </Provider>
+    </Fragment>
+  </React.StrictMode>
+);
+
+reportWebVitals();
